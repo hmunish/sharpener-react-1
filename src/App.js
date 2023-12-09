e43a6784a21b59284b7c5a7065a49580af61143d
Munish <hmunish5@gmail.com>
2023-12-09 17:55:19 +0200

    Add user form submit handling

--- src/App.js
+++ src/App.js
@@ -5,20 +5,32 @@ import UserDetailCard from './components/user/UserDetailCard';
 import Notification from './components/Notification';
 
 function App() {
-  const [users, setUsers] = useState([{username: "munish", age: 50}, {username: "munish", age: 50}]);
+  const [users, setUsers] = useState([]);
   const [error, setError] = useState({isError: false, message: ""});
+
+  const formHandler = (e) => {
+    e.preventDefault();
+    const {username, age} = e.target;
+    console.log(username.value, +age.value);
+
+    if(!username.value || !age.value) return setError({isError: true, message: "Empty fields"})
+    if(+age.value < 0) return setError({isError: true, message: "Age should be greater than zero"});
+
+    setError({isError: false, message: ""});
+
+    setUsers((prevValue) => [...prevValue, {username: username.value, age: age.value}]);
+  }
 
   const usersContent = !users.length ? <p className="text-body-secondary fw-semibold">No users, please add to see here</p> : users.map(user => <UserDetailCard key={user.username} username = {user.username} age = {user.age} />)
   return (
     <main id="app" className='container-fluid bg-dark vh-100'>
      {error.isError && <Notification message={error.message} />}
     <section className='w-50 bg-light m-auto p-3 rounded'>
-    <UserForm />
+    <UserForm submitHandler = {formHandler}/>
     </section>
     <section className='w-50 m-auto rounded bg-light p-3 mt-5'>
       {usersContent}
     </section>
-
    </main>
   );
 }
--- src/components/user/UserForm.js
+++ src/components/user/UserForm.js
@@ -1,10 +1,10 @@
-const UserForm = () => {
+const UserForm = (props) => {
     return (
-        <form>
+        <form onSubmit={props.submitHandler}>
             <label className="form-label">Username</label>
-            <input type="text" className="form-control"/>
+            <input type="text" name="username" className="form-control"/>
             <label className="form-label">Age (Years)</label>
-            <input type="number" className="form-control"/>
+            <input type="number" name="age" className="form-control"/>
             <button type="submit" className="btn btn-primary mt-3 c-btn-1">Submit</button>   
         </form>
     )
